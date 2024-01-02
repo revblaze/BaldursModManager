33edd08a27eeaa603bf087a7f89f1405db2873c6
Justin Bush <jubush@icloud.com>
2024-01-02 12:01:44 -0700

    feat: SwiftUI base project files

---
+++ BaldursModManager/BaldursModManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaldursModManagerApp.swift
+//  BaldursModManager
+//
+//  Created by Justin Bush on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BaldursModManagerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
