19f1f3db3e29a9e89d13a54e116caf3275cf1ee1
Justin Bush <jubush@icloud.com>
2024-01-03 16:09:31 -0700

    feat: copy/move mod to application support for handling

--- BaldursModManager/ContentView.swift
+++ BaldursModManager/ContentView.swift
@@ -11,6 +11,10 @@ import AppKit
 struct ContentView: View {
   @State private var modItems: [ModItem] = []
   
+  init() {
+    createUserModsFolderIfNeeded()
+  }
+  
   var body: some View {
     VStack {
       List {
@@ -33,6 +37,11 @@ struct ContentView: View {
         //Text("Baldur's Mod Manager").font(.headline)
       }
       ToolbarItemGroup(placement: .automatic) {
+        Button(action: {
+          openUserModsFolder()
+        }) {
+          Label("Open UserMods", systemImage: "folder")
+        }
         Button(action: {
           selectFile()
         }) {
@@ -66,7 +75,7 @@ struct ContentView: View {
       let fileManager = FileManager.default
       let contents = try fileManager.contentsOfDirectory(atPath: url.path)
       Debug.log("Directory contents: \(contents)")
-
+      
       if let infoJsonUrl = contents.first(where: { $0.caseInsensitiveCompare("info.json") == .orderedSame }) {
         let fullPath = url.appendingPathComponent(infoJsonUrl).path
         readAndLogJsonContents(atPath: fullPath, directoryContents: contents)
@@ -81,14 +90,18 @@ struct ContentView: View {
       let data = try Data(contentsOf: URL(fileURLWithPath: path))
       if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
         Debug.log("Contents of info.json: \(json)")
-
+        
         if let modsArray = json["Mods"] as? [[String: Any]], let md5 = json["MD5"] as? String {
           let mods = modsArray.compactMap { modDict -> Mod? in
             var mod = try? JSONDecoder().decode(Mod.self, from: JSONSerialization.data(withJSONObject: modDict))
             mod?.md5 = md5
             return mod
           }
-          let newModItem = ModItem(directoryPath: path, directoryContents: directoryContents, mods: mods)
+          let directoryURL = URL(fileURLWithPath: path).deletingLastPathComponent() // Getting parent directory path
+          let newModItem = ModItem(directoryPath: directoryURL.path, directoryContents: directoryContents, mods: mods)
+          
+          handleModFolder(at: directoryURL, for: newModItem)
+          
           DispatchQueue.main.async {
             self.modItems.append(newModItem)
           }
@@ -98,7 +111,6 @@ struct ContentView: View {
       Debug.log("Error reading JSON file: \(error)")
     }
   }
-
   
   private func addModItem(directory: URL, directoryContents: [String], mods: [Mod]) {
     let newItem = ModItem(directoryPath: directory.path, directoryContents: directoryContents, mods: mods)
@@ -107,6 +119,50 @@ struct ContentView: View {
   
   private func move(from source: IndexSet, to destination: Int) {
     modItems.move(fromOffsets: source, toOffset: destination)
+  }
+  
+  private func handleModFolder(at originalPath: URL, for modItem: ModItem) {
+    let fileManager = FileManager.default
+    if let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+      let destinationURL = appSupportURL.appendingPathComponent("UserMods").appendingPathComponent(originalPath.lastPathComponent)
+      
+      do {
+        if UserSettings.shared.makeCopyOfModFolderOnImport {
+          try fileManager.copyItem(at: originalPath, to: destinationURL)
+        } else {
+          try fileManager.moveItem(at: originalPath, to: destinationURL)
+        }
+        var updatedModItem = modItem
+        updatedModItem.directoryPath = destinationURL.path
+        DispatchQueue.main.async {
+          self.modItems.append(updatedModItem)
+        }
+      } catch {
+        Debug.log("Error handling mod folder: \(error)")
+      }
+    }
+  }
+  
+  private func openUserModsFolder() {
+    if let appSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+      let userModsURL = appSupportURL//.appendingPathComponent("UserMods")
+      NSWorkspace.shared.open(userModsURL)
+    }
+  }
+  
+  private func createUserModsFolderIfNeeded() {
+    let fileManager = FileManager.default
+    if let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+      let userModsURL = appSupportURL.appendingPathComponent("UserMods")
+      
+      if !fileManager.fileExists(atPath: userModsURL.path) {
+        do {
+          try fileManager.createDirectory(at: userModsURL, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+          Debug.log("Error creating UserMods directory: \(error)")
+        }
+      }
+    }
   }
 }
 
---
+++ BaldursModManager/Utilities/FileManager.swift
@@ -0,0 +1,11 @@
+//
+//  FileManager.swift
+//  BaldursModManager
+//
+//  Created by Justin Bush on 1/3/24.
+//
+
+import Foundation
+
+
+
