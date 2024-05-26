a4d31f01fa05c2ff73f5d20bd9463469b76fca6c
Justin Bush <jdevbush@gmail.com>
2024-05-26 16:28:20 -0600

    feat: SFSymbol, SymbolButtons

---
+++ BaldursModManager/Views/Symbols/SFSymbol.swift
@@ -0,0 +1,94 @@
+//
+//  SFSymbol.swift
+//  ObservableWebView
+//
+//  Created by Justin Bush on 3/15/24.
+//
+
+import Foundation
+
+enum SFSymbol: String {
+  case coreMl = "rotate.3d"
+  case python = "point.bottomleft.forward.to.point.topright.scurvepath"
+  case network
+  case arkit
+  case gear
+  
+  case circleFill = "circle.fill"
+  case circleDotted = "circle.dotted"
+  case circleDashed = "circle.dashed"
+  
+  case play = "play.fill"
+  case stop = "stop.fill"
+  case menuPlay = "play"
+  case menuStop = "stop"
+  case bonjour
+  case trash
+  case lock
+  case star
+  
+  case code = "chevron.left.forwardslash.chevron.right"
+  
+  case solidLock = "lock.fill"
+  case solidLockSlash = "lock.slash.fill"
+  case search = "magnifyingglass"
+  
+  case folder
+  
+  case tabBar = "character.textbox"
+  case tabGallery = "square.on.square"
+  
+  case checkmark
+  case pencil
+  
+  case safari
+  case download = "arrow.down.circle"
+  case addNewCircle = "plus.circle"
+  case profile = "person.crop.circle"
+  
+  case newFolder = "folder.badge.plus"
+  case newPrompt = "plus.bubble"
+  
+  case upDirectory = "arrow.turn.left.up"
+  
+  case copy = "clipboard"
+  case paste = "arrow.up.doc.on.clipboard"
+  
+  case closeFullscreen = "xmark.circle.fill"
+  case clock
+  
+  case close = "xmark"
+  case save = "square.and.arrow.down"
+  case copyToWorkspace = "tray.and.arrow.up"
+  
+  case shuffle
+  case repeatLast = "repeat"
+  
+  // Detail
+  case photo
+  case back = "arrow.left"
+  case forward = "arrow.right"
+  case refresh = "arrow.clockwise"
+  case mostRecent = "clock.arrow.circlepath"
+  case fullscreen = "arrow.up.left.and.arrow.down.right"
+  case share = "square.and.arrow.up"
+  
+  case help = "questionmark.circle"
+  case warning = "exclamationmark.triangle"
+  case forceStop = "xmark.octagon"
+  
+  case none
+}
+
+import SwiftUI
+
+extension SFSymbol {
+  var name: String {
+    return self.rawValue
+  }
+  
+  var image: some View {
+    Image(systemName: self.name)
+      .resizable()
+  }
+}
---
+++ BaldursModManager/Views/Symbols/SymbolButtons.swift
@@ -0,0 +1,95 @@
+//
+//  SymbolButtons.swift
+//  ObservableWebView
+//
+//  Created by Justin Bush on 3/15/24.
+//
+
+import SwiftUI
+
+struct MenuButton: View {
+  let title: String
+  var symbol: SFSymbol?
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: action) {
+      HStack {
+        if let symbol = symbol {
+          symbol.image
+        }
+        Text(title)
+      }
+    }
+  }
+}
+
+struct ToggleMenuButton: View {
+  let title: String
+  var toggle: Bool
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: action) {
+      HStack {
+        if toggle {
+          SFSymbol.checkmark.image
+        }
+        Text(title)
+      }
+    }
+  }
+}
+
+struct SymbolButton: View {
+  let symbol: SFSymbol
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: {
+      action()
+    }) {
+      symbol.image
+    }
+    .buttonStyle(BorderlessButtonStyle())
+  }
+}
+
+struct AccessorySymbolButton: View {
+  let symbol: SFSymbol
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: {
+      action()
+    }) {
+      symbol.image
+    }
+    .buttonStyle(.accessoryBar)
+  }
+}
+
+struct ToolbarSymbolButton: View {
+  let title: String
+  let symbol: SFSymbol
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: {
+      action()
+    }) {
+      Label(title, systemImage: symbol.name)
+    }
+  }
+}
+
+
+struct SymbolButtons: View {
+  var body: some View {
+    VStack(spacing: 10) {
+      MenuButton(title: "Test", symbol: .python, action: {})
+      SymbolButton(symbol: .python, action: {})
+    }
+    .padding()
+  }
+}
