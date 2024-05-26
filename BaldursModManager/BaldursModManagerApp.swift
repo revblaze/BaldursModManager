b48c23d7e77d70b9353838847de524e969786582
Justin Bush <jdevbush@gmail.com>
2024-05-26 17:56:33 -0600

    refactor: app main, environment, hierarchy

--- BaldursModManager/BaldursModManagerApp.swift
+++ BaldursModManager/BaldursModManagerApp.swift
@@ -10,7 +10,41 @@ import SwiftData
 
 @main
 struct BaldursModManagerApp: App {
+  @Environment(\.global) var global
   var container: ModelContainer
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .frame(minWidth: 600, idealWidth: 900, minHeight: 400, idealHeight: 650)
+        .environment(global)
+        .onAppear {
+          NSWindow.allowsAutomaticWindowTabbing = false
+        }
+    }
+    .modelContainer(container)
+    .commands {
+      CommandGroup(before: .appSettings) {
+        Button("Settings...") {
+          global.showSettingsView = true
+        }
+        .keyboardShortcut(",", modifiers: [.command])
+      }
+      CommandGroup(replacing: .newItem) {}
+      CommandGroup(before: .importExport) {
+        Button("Import Mod...") {
+          global.showImportModPanel = true
+        }
+        .keyboardShortcut("O", modifiers: [.command])
+      }
+      
+      CommandMenu("Debug") {
+        Button(Debug.shared.isActive ? "Disable Debug" : "Enable Debug") {
+          Debug.shared.isActive.toggle()
+        }
+      }
+    }
+  }
   
   init() {
     do {
@@ -21,8 +55,6 @@ struct BaldursModManagerApp: App {
       let storeURL = appSupportURL
         .appendingPathComponent(Constants.ApplicationSupportFolderName)
         .appendingPathComponent(Constants.ApplicationSwiftDataFileName)
-      
-      // Create the subfolder if it doesn't exist
       let subfolderURL = storeURL.deletingLastPathComponent()
       if !fileManager.fileExists(atPath: subfolderURL.path) {
         try fileManager.createDirectory(at: subfolderURL, withIntermediateDirectories: true)
@@ -32,32 +64,6 @@ struct BaldursModManagerApp: App {
       container = try ModelContainer(for: ModItem.self, configurations: config)
     } catch {
       fatalError("Failed to configure SwiftData container: \(error)")
-    }
-  }
-  
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
-        .frame(minWidth: 600, idealWidth: 1200, minHeight: 400, idealHeight: 900)
-        .onAppear {
-          NSWindow.allowsAutomaticWindowTabbing = false
-        }
-    }
-    .modelContainer(container)
-    .commands {
-      CommandGroup(replacing: .newItem) {}
-      CommandGroup(before: .importExport) {
-        Button("Import Mod...") {
-          print("import")
-        }
-        .keyboardShortcut("O", modifiers: [.command])
-      }
-      
-      CommandMenu("Debug") {
-        Button(Debug.shared.isActive ? "Disable Debug" : "Enable Debug") {
-          Debug.shared.isActive.toggle()
-        }
-      }
     }
   }
 }
