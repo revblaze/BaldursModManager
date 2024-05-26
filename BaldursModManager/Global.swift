7d6f8aa2f1917a46e00cf1ab60afc9cd6c62859b
Justin Bush <jdevbush@gmail.com>
2024-05-26 17:24:01 -0600

    feat: Global

---
+++ BaldursModManager/Global.swift
@@ -0,0 +1,26 @@
+//
+//  GlobalEnvironment.swift
+//  BaldursModManager
+//
+//  Created by Justin Bush on 5/26/24.
+//
+
+import SwiftUI
+import Observation
+
+@Observable
+class Global {
+  var showSettingsView: Bool = false
+  var showImportModPanel: Bool = false
+}
+
+struct GlobalKey: EnvironmentKey {
+  static let defaultValue: Global = Global()
+}
+
+extension EnvironmentValues {
+  var global: Global {
+    get { self[GlobalKey.self] }
+    set { self[GlobalKey.self] = newValue }
+  }
+}
