ae9f1f09766e855d5b53a47f82a03926b67e30fa
Justin Bush <jdevbush@gmail.com>
2024-05-27 13:14:57 -0600

    refactor: UpdateView for sheet presentation

--- BaldursModManager/Views/Update/UpdateView.swift
+++ BaldursModManager/Views/Update/UpdateView.swift
@@ -35,139 +35,149 @@ struct UpdateView: View {
   @State var updateViewState: UpdateViewState = .defaultState
   
   var body: some View {
-    VStack {
-      HStack {
-        SymbolButton(symbol: .closeFullscreen) {
-          isPresented = false
-        }
-        .frame(width: 20, height: 20)
-        Spacer()
-        
-        SymbolButton(symbol: .clock) {
-          withAnimation {
-            showUpdateFrequencySection.toggle()
-          }
-        }
-        .frame(width: 20, height: 20)
-        .foregroundStyle(showUpdateFrequencySection ? .blue : .secondary)
-      }
-      
-      ToggleWithLabel(isToggled: .constant(true), header: "Automatically check for new updates", description: "Checks for new releases on GitHub", showAllDescriptions: true)
-        .padding(.top, 10)
-      
-      if showUpdateFrequencySection {
+    GeometryReader { geometry in
+      ZStack {
         VStack {
-          Menu {
-            ForEach(UpdateFrequency.allCases, id: \.self) { frequency in
-              Button(frequency.rawValue) {
-                updateManager.updateCheckFrequency = frequency
-                updateManager.saveSettings()
+          Spacer().frame(height: 30) // Placeholder for top HStack
+
+          ToggleWithLabel(isToggled: .constant(true), header: "Automatically check for new updates", description: "Checks if new releases are available", showAllDescriptions: true)
+            .padding(.top, 10)
+          
+          if showUpdateFrequencySection {
+            VStack {
+              Menu {
+                ForEach(UpdateFrequency.allCases, id: \.self) { frequency in
+                  Button(frequency.rawValue) {
+                    updateManager.updateCheckFrequency = frequency
+                    updateManager.saveSettings()
+                  }
+                }
+              } label: {
+                Label("Check " + updateManager.updateCheckFrequency.rawValue, systemImage: "calendar")
               }
             }
-          } label: {
-            Label("Check " + updateManager.updateCheckFrequency.rawValue, systemImage: "calendar")
+            .frame(width: 250, height: updateFrequencySectionHeight)
+            .transition(.asymmetric(insertion: .move(edge: .top).combined(with: .opacity),
+                                    removal: .move(edge: .top).combined(with: .opacity)))
           }
-        }
-        .frame(width: 250, height: updateFrequencySectionHeight)
-        .transition(.asymmetric(insertion: .move(edge: .top).combined(with: .opacity),
-                                removal: .move(edge: .top).combined(with: .opacity)))
-      }
-      
-      Spacer()
-      
-      HStack(alignment: .center) {
-        VStack {
-          if updateViewState == .checkingForUpdate {
-            ProgressView()
-              .progressViewStyle(CircularProgressViewStyle())
-              .scaleEffect(0.5)
-              .opacity(updateViewState == .checkingForUpdate ? 1 : 0)
-          } else {
-            Image(systemName: updateViewState.symbol)
-              .foregroundStyle(updateViewState.symbolColor)
-              .font(.system(size: 18))
-          }
-        }
-        .frame(width: 24, height: 24)
-        .padding(.trailing, 2)
           
-        
-        VStack(alignment: .leading) {
-          Text(updateViewState.statusText)
-            .bold()
-            .padding(.bottom, 1)
-          Text("BaldursModManager \(AppInfo.versionAndBuild)")
-            .font(.system(size: 12, weight: .medium))
-            .foregroundStyle(.secondary)
-        }
-      }
-      
-      Spacer()
-      
-      HStack {
-        if updateViewState == .newVersionAvailable {
-          OutlineButton(title: "Open Releases") {
-            if let releasesUrl = URL(string: Constants.releasesUrl) {
-              NSWorkspace.shared.open(releasesUrl)
+          Spacer()
+          
+          HStack(alignment: .center) {
+            VStack {
+              if updateViewState == .checkingForUpdate {
+                ProgressView()
+                  .progressViewStyle(CircularProgressViewStyle())
+                  .scaleEffect(0.5)
+                  .opacity(updateViewState == .checkingForUpdate ? 1 : 0)
+              } else {
+                Image(systemName: updateViewState.symbol)
+                  .foregroundStyle(updateViewState.symbolColor)
+                  .font(.system(size: 18))
+              }
+            }
+            .frame(width: 24, height: 24)
+            .padding(.trailing, 2)
+            
+
+            VStack(alignment: .leading) {
+              Text(updateViewState.statusText)
+                .bold()
+                .padding(.bottom, 1)
+              Text("BaldursModManager \(AppInfo.versionAndBuild)")
+                .font(.system(size: 12, weight: .medium))
+                .foregroundStyle(.secondary)
             }
           }
-          BlueButton(title: "Download Now") {
-            if let latestRelease = updateManager.latestRelease, let releaseUrl = latestRelease.releaseDownloadUrlString, let url = URL(string: releaseUrl) {
-              NSWorkspace.shared.open(url)
-            }
-          }
-        } else {
-          OutlineButton(title: updateViewState.mainButtonText) {
+          
+          Spacer()
+          
+          HStack {
             if updateViewState == .newVersionAvailable {
-              if let latestRelease = updateManager.latestRelease, let releaseUrl = latestRelease.releaseDownloadUrlString, let url = URL(string: releaseUrl) {
-                NSWorkspace.shared.open(url)
+              OutlineButton(title: "Open Releases") {
+                if let releasesUrl = URL(string: Constants.releasesUrl) {
+                  NSWorkspace.shared.open(releasesUrl)
+                }
+              }
+              BlueButton(title: "Download Now") {
+                if let latestRelease = updateManager.latestRelease, let releaseUrl = latestRelease.releaseDownloadUrlString, let url = URL(string: releaseUrl) {
+                  NSWorkspace.shared.open(url)
+                }
               }
             } else {
-              Task {
-                await updateManager.checkForUpdatesIfNeeded(force: true)
+              OutlineButton(title: updateViewState.mainButtonText) {
+                if updateViewState == .newVersionAvailable {
+                  if let latestRelease = updateManager.latestRelease, let releaseUrl = latestRelease.releaseDownloadUrlString, let url = URL(string: releaseUrl) {
+                    NSWorkspace.shared.open(url)
+                  }
+                } else {
+                  Task {
+                    await updateManager.checkForUpdatesIfNeeded(force: true)
+                  }
+                }
               }
             }
+            
           }
+          .padding(.bottom, 10)
+          .disabled(updateManager.isCheckingForUpdate)
+          .onChange(of: updateManager.isCheckingForUpdate) {
+            updateViewStateBasedOnManager()
+          }
+          .onChange(of: updateManager.currentBuildIsLatestVersion) {
+            updateViewStateBasedOnManager()
+          }
+          
+          HStack {
+            if let lastChecked = updateManager.lastCheckedTimestamp {
+              Text("Last checked: \(lastChecked, formatter: itemFormatter)")
+                .font(.footnote)
+                .foregroundStyle(Color.secondary)
+            } else {
+              Text("Last checked: Never")
+                .font(.footnote)
+                .foregroundStyle(Color.secondary)
+            }
+          }
+          .padding(.bottom,  2)
+          
+          if let checkForUpdateError = updateManager.checkForUpdatesErrorMessage {
+            Text(checkForUpdateError)
+              .padding(.vertical, 4)
+              .font(.footnote)
+              .foregroundStyle(Color.red)
+              .onAppear {
+                Delay.by(7) { updateManager.checkForUpdatesErrorMessage = nil }
+              }
+          }
+        }
+        .padding()
+        .frame(width: 400, height: showUpdateFrequencySection ? expandedFrameHeight : initialFrameHeight)
+        .animation(.easeInOut, value: showUpdateFrequencySection)
+        .onAppear {
+          updateViewStateBasedOnManager()
         }
         
-      }
-      .padding(.bottom, 10)
-      .disabled(updateManager.isCheckingForUpdate)
-      .onChange(of: updateManager.isCheckingForUpdate) {
-        updateViewStateBasedOnManager()
-      }
-      .onChange(of: updateManager.currentBuildIsLatestVersion) {
-        updateViewStateBasedOnManager()
-      }
-      
-      HStack {
-        if let lastChecked = updateManager.lastCheckedTimestamp {
-          Text("Last checked: \(lastChecked, formatter: itemFormatter)")
-            .font(.footnote)
-            .foregroundStyle(Color.secondary)
-        } else {
-          Text("Last checked: Never")
-            .font(.footnote)
-            .foregroundStyle(Color.secondary)
-        }
-      }
-      .padding(.bottom,  2)
-      
-      if let checkForUpdateError = updateManager.checkForUpdatesErrorMessage {
-        Text(checkForUpdateError)
-          .padding(.vertical, 4)
-          .font(.footnote)
-          .foregroundStyle(Color.red)
-          .onAppear {
-            Delay.by(7) { updateManager.checkForUpdatesErrorMessage = nil }
+        HStack {
+          SymbolButton(symbol: .closeFullscreen) {
+            isPresented = false
           }
+          .frame(width: 20, height: 20)
+          Spacer()
+          
+          SymbolButton(symbol: .clock) {
+            withAnimation {
+              showUpdateFrequencySection.toggle()
+            }
+          }
+          .frame(width: 20, height: 20)
+          .foregroundStyle(showUpdateFrequencySection ? .blue : .secondary)
+        }
+        .padding()
+        .padding(.top, 24)
+        .frame(width: geometry.size.width)
+        .position(x: geometry.size.width / 2, y: 15)
       }
-    }
-    .padding()
-    .frame(width: 400, height: showUpdateFrequencySection ? expandedFrameHeight : initialFrameHeight)
-    .animation(.easeInOut, value: showUpdateFrequencySection)
-    .onAppear {
-      updateViewStateBasedOnManager()
     }
     .navigationTitle("Check for Updates")
   }
